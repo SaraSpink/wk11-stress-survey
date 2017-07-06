09ea82aa0c18cadfe64537deab4d24ecd4d014f6
Sara Spink and Tory Bryant <sara@>
2017-07-05 20:42:51 -0700

    add a returned response in the javaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,42 @@
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function(){
-  $("form#transportation_survey").submit(function(event){
+  $("form#stress-survey").submit(function(event){
     event.preventDefault();
-    $("#work-responses").show();
-    var userResponses = [];
-    $("input:checkbox[name=work-transportation]:checked").each(function(){
-      var workTransportationMode = $(this).val();
-      $('#work-responses').append(workTransportationMode + "<br>");
-        userResponses.push(workTransportationMode);
+    $("#warning-responses").show();
+    // var userResponses = [];
+    var warningSignsCounter = 0;
+    $("input:checkbox[name=warning-signs]:checked").each(function(){
+      warningSignsCounter += 1;
+      if (warningSignsCounter === 3) {
+        return $('#warning-responses').append("you seem stressed. Call doctor feelgood!");
+      } else {
+        return $('#warning-responses').append("you are feeling fine!");
+      }
+      // var warningSigns = $(this).val();
+      // $('#warning-responses').append(warningSigns + "<br>");
+        // userResponses.push(workTransportationMode);
     });
-    $("#fun-responses").show();
-    $("input:checkbox[name=fun-transportation]:checked").each(function(){
-      var funTransportationMode = $(this).val();
-      $('#fun-responses').append(funTransportationMode + "<br>");
-      userResponses.push(funTransportationMode);
+    $("#health-responses").show();
+    $("input:checkbox[name=health-symptons]:checked").each(function(){
+      var healthSymptoms = $(this).val();
+      $('#health-responses').append(healthSymptoms + "<br>");
+      // userResponses.push(funTransportationMode);
+    });
+
+    $("#coping-responses").show();
+    $("input:checkbox[name=coping]:checked").each(function(){
+      var coping = $(this).val();
+      $('#coping-responses').append(coping + "<br>");
     });
     // var userResponses = [];
     // $("input:checkbox[name=work-transportation]:checked").each(function(){
@@ -24,6 +48,6 @@ $(document).ready(function(){
     //   userResponses.push(funTransportationMode);
     // });
     console.log(userResponses);
-    $('#transportation_survey').hide();
+    $('#stress-survey').hide();
   });
 });
